d625baa2dba98ce8cf73a83c797303de2878c641
mutalisk999 <81849682@qq.com>
2024-09-09 10:33:03 +0800

    optimize

--- goBlakeWrapper_test.go
+++ goBlakeWrapper_test.go
@@ -5,18 +5,20 @@ import (
 	"testing"
 )
 
+// 776484204c66ec4894d5a3879aeddb3772cac5fc2795ed26d9ef2c68f73764cc
 func TestCalcDecredHashHex(t *testing.T) {
-	input := make([]byte, 180)
-	for i := 0; i < 180; i++ {
+	input := make([]byte, 40)
+	for i := 0; i < 40; i++ {
 		input[i] = 0x0
 	}
 	output := CalcDecredHashHex(input)
 	fmt.Println(output)
 }
 
+// 4a1931803561f431decab002e7425f0a8531d5e456a1a47fd9998a2530c0f800
 func TestCalcSiaHashHex(t *testing.T) {
-	input := make([]byte, 80)
-	for i := 0; i < 80; i++ {
+	input := make([]byte, 40)
+	for i := 0; i < 40; i++ {
 		input[i] = 0x0
 	}
 	output := CalcSiaHashHex(input)
