a3ae88217fc877441bb4aaa68a5a4a17d0efcf6e
mutalisk999 <81849682@qq.com>
2024-09-23 12:02:41 +0800

    update

--- blake_hash.cpp
+++ blake_hash.cpp
@@ -7,12 +7,6 @@
 #include <memory.h>
 
 
-#define A 64
-
-#ifndef _MSC_VER
-#define _ALIGN(x) __attribute__ ((aligned(x)))
-#endif
-
 
 void decred_hash(char *state, const char *input, unsigned int len)
 {
@@ -29,7 +23,7 @@ void decred_hash(char *state, const char *input, unsigned int len)
 
 void sia_hash(char *output, const char *input, unsigned int len)
 {
-	uint8_t _ALIGN(A) hash[32];
+	uint8_t hash[32];
 	blake2b_ctx ctx;
 
 	blake2b_init(&ctx, 32, NULL, 0);
@@ -41,27 +35,27 @@ void sia_hash(char *output, const char *input, unsigned int len)
 
 // decred f8c5ea75d4d26911fd8ec3edace55b069f01212f581541fcac0c815ca83e83b3
 // sia 143aa0da2b6a4ca39eee3ee50a6536d75eedff3b5ef0229a6d603afa7854d5b8
-//int main() {
-//	char i1[192];
-//	char *in1 = i1;
-//	for(int i = 0; i < 192; ++i)
-//	    i1[i] = char(0x0);
-//
-//	char o1[32];
-//	char *out1 = o1;
-//
-//
-//	decred_hash(out1, in1, 180);
-//	for(int i = 0; i < 32; ++i)
-//		printf("%02x", (unsigned char)o1[i]);
-//	printf("\n");
-//
-//    char o2[32];
-//    char *out2 = o2;
-//
-//
-//    sia_hash(out2, in1, 80);
-//    for(int i = 0; i < 32; ++i)
-//        printf("%02x", (unsigned char)o2[i]);
-//    printf("\n");
-//}
+int main() {
+	char i1[192];
+	char *in1 = i1;
+	for(int i = 0; i < 192; ++i)
+	    i1[i] = char(0x0);
+
+	char o1[32];
+	char *out1 = o1;
+
+
+	decred_hash(out1, in1, 180);
+	for(int i = 0; i < 32; ++i)
+		printf("%02x", (unsigned char)o1[i]);
+	printf("\n");
+
+    char o2[32];
+    char *out2 = o2;
+
+
+    sia_hash(out2, in1, 80);
+    for(int i = 0; i < 32; ++i)
+        printf("%02x", (unsigned char)o2[i]);
+    printf("\n");
+}
